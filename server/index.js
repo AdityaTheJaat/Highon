61b19119738d62463aa606d8352a44187f6840ba
AdityaTheJaat <programmingID1306@gamil.com>
2023-07-26 15:30:56 +0530

    post

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cloudinary = require("cloudinary").v2;
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 
@@ -27,8 +28,19 @@ mongoose
   });
 
 
-
-
+  const cloudinaryConnect = () => {
+    try {
+      cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+      });
+      console.log("Cloudinary connected successfully!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  cloudinaryConnect();
 
   //getUser 
   //getPosts 
